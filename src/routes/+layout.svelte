2f89bf2498cb8d1081be1d575aa44d8788d4d7f2
dan_immense <dan.nisbet@immense.ai>
2023-04-03 09:56:28 +0100

    feat: add complete route geojson

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import { onMount } from 'svelte';
-	import { route } from '../stores/route.store';
-	import type { RouteGeoJson } from '../stores/route.store.d';
+	import { route } from '$lib/stores/route.store';
+	import type { RouteGeoJson } from '$lib/stores/route.store.d';
 
 	onMount(async () => {
-		const res = await fetch('data/frog-graham.geo.json');
+		const res = await fetch('data/frog-graham-cw.geo.json');
 		const data: RouteGeoJson = await res.json();
 		route.set(data);
 	});
