a9d5bede7f56e352152567af9a76da6e5f386870
Dan Nisbet <dan.nisbet@immense.ai>
2023-05-15 22:53:04 +0100

    feat: add meta seo tags

--- src/routes/[round=name]/+page.svelte
+++ src/routes/[round=name]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import type { PageData } from './$types';
 	import { beforeUpdate } from 'svelte';
 	import { breakdown } from '$lib/stores/breakdown.store';
@@ -14,6 +15,21 @@
 		route.set(routeGeoJson);
 	});
 </script>
+<svelte:head>
+	<title>{`Round Runner: Validate Your ${data.title} ${data.description}`}</title>
+	<meta name="description" content={`Round Runner is an online tool designed to verify and validate your long-distance ${data.description} ${data.title}`}>
+	<meta name="keywords" content="Round Runner, Running Verification, Long-Distance Running, Running Rounds, Online Tool, Run Tracking, Distance Verification, Performance Optimization, Frog Graham Round, Tadpole Round, SwimRun Challenge, Swim Run">
+	
+	<meta property="og:type" content="website">
+	<meta property="og:url" content={$page.url.href}>
+	<meta property="og:title" content={`Verify Your ${data.title}`}>
+	<meta property="og:description" content="Round Runner is an innovative online tool designed to verify and validate your long-distance running rounds. Accurately track and optimize your running performance with our cutting-edge technology.">
+	
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content={$page.url.href}>
+	<meta property="twitter:title" content={`Verify Your ${data.title}`}>
+	<meta property="twitter:description" content="Round Runner is an innovative online tool designed to verify and validate your long-distance running rounds. Accurately track and optimize your running performance with our cutting-edge technology.">
+</svelte:head>
 
 <div
 	class="
