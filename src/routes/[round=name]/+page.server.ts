072e23a899c0718af78124797de8695b136e6c23
Dan Nisbet <dan.nisbet@immense.ai>
2023-06-15 14:28:29 +0100

    fix: production SSR 404 error

--- src/routes/[round=name]/+page.server.ts
+++ src/routes/[round=name]/+page.server.ts
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-export const prerender = true;
+export const prerender = 'auto';
 export const load = (async ({ params, parent }) => {
 	const { rounds } = await parent();
 	const matchingRound = rounds.find((round) => round.slug === params.round);
