fdce25203385766a6f001bdead3304ce85f7b653
Dan Nisbet <dan.nisbet@immense.ai>
2024-10-03 23:38:48 +0100

    chore: stye linting

--- src/components/Checker/components/Expander.svelte
+++ src/components/Checker/components/Expander.svelte
@@ -12,8 +12,8 @@
 	<div
 		class="
 			relative overflow-y-auto
-			before:pointer-events-none 
-			before:z-20 before:fixed before:left-0 before:right-4 before:h-16 
+			before:pointer-events-none
+			before:z-20 before:fixed before:left-0 before:right-4 before:h-16
 			before:bg-gradient-to-b before:from-white before:via-white
 			print:before:hidden
 			before:hidden md:before:block
@@ -26,7 +26,7 @@
 		</div>
 		<div
 			class="
-				fixed bottom-0 left-0 right-4 h-5 
+				fixed bottom-0 left-0 right-4 h-5
 				bg-gradient-to-t from-white
 				pointer-events-none
 				print:hidden hidden md:block
