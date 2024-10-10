6f11a1d1e309c02c4ff7e7972fcffed1619b4361
Dan Nisbet <dan.nisbet@immense.ai>
2024-10-10 14:14:44 +0100

    feat: sitemap.xml

--- src/components/Checker/Checker.svelte
+++ src/components/Checker/Checker.svelte
@@ -1,23 +1,62 @@
 <script lang="ts">
-	import { onDestroy } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
+	import { pushState } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
 	import RouteSelector from '../RouteSelector/RouteSelector.svelte';
 	import { Upload, Checkpoints, Expander, Results, ExpandAction } from './components';
 	import { gpx } from '$lib/stores/gpx.store';
 	import { breakdown } from '$lib/stores/breakdown.store';
 	import { isOpen } from '$lib/stores/checker.store';
 
+	const ROUTE_INFORMATION_PARAM = 'route-information';
 	let hasGpx = false;
+	let urlParams = browser ? new URLSearchParams(window.location.search) : new URLSearchParams();
+
+	function handlePopState() {
+		console.log('handlePopState', $page.state.ROUTE_INFORMATION_PARAM);
+		$isOpen = $page.state.ROUTE_INFORMATION_PARAM;
+	}
+
+	function getParams() {
+		let newUrl = '';
+		if (browser) {
+			let params = new URLSearchParams();
+			if ($isOpen) params.set(ROUTE_INFORMATION_PARAM, 'open');
+			else params.delete(ROUTE_INFORMATION_PARAM);
+			newUrl = `${window.location.pathname}?${params.toString()}`;
+		}
+		return newUrl;
+	}
 
 	function handleOnClick() {
 		$isOpen = !$isOpen;
+
+		const newUrl = getParams();
+		pushState(newUrl, {
+			ROUTE_INFORMATION_PARAM: $isOpen
+		});
 	}
 
-	const unsubscribe = gpx.subscribe((geojson) => {
+	const unsubscribeGpx = gpx.subscribe((geojson) => {
 		hasGpx = geojson.features.length > 0;
 		$isOpen = hasGpx;
 	});
 
-	onDestroy(unsubscribe);
+	onMount(() => {
+		$isOpen = $page.state.ROUTE_INFORMATION_PARAM;
+		if (browser) {
+			$isOpen = urlParams.has(ROUTE_INFORMATION_PARAM);
+			window.addEventListener('popstate', handlePopState);
+		}
+	});
+
+	onDestroy(() => {
+		unsubscribeGpx();
+		if (browser) {
+			window.removeEventListener('popstate', handlePopState);
+		}
+	});
 </script>
 
 <div class="md:h-screen print:h-auto">
