9faee99366338b23f6d5ab0730c046dfbc9f95bb
Dan Nisbet <dan.nisbet@immense.ai>
2024-10-11 21:57:45 +0100

    style: text color tweaks

--- src/components/Checker/components/ExpandAction.svelte
+++ src/components/Checker/components/ExpandAction.svelte
@@ -10,7 +10,7 @@
 <button
 	on:click={handleOnClick}
 	type="button"
-	class={`relative z-30 border ${buttonClass} focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-1 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500`}
+	class={`relative z-30 border ${buttonClass} focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-1.5 text-center inline-flex items-center`}
 >
 	<svg
 		aria-hidden="true"
