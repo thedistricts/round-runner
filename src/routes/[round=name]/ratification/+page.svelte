f67d9435c3ad6bfd47158d77d06a69c2643300d9
Dan Nisbet <dan.nisbet@immense.ai>
2025-11-14 23:17:42 +0000

    fix: disable redirect

--- src/routes/[round=name]/ratification/+page.svelte
+++ src/routes/[round=name]/ratification/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { get } from 'svelte/store';
-	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
+	// import { get } from 'svelte/store';
+	// import { goto } from '$app/navigation';
+	// import { page } from '$app/stores';
 	import { isOpen } from '$lib/stores/checker.store';
-	import { gpx } from '$lib/stores/gpx.store';
+	// import { gpx } from '$lib/stores/gpx.store';
 	import { Results } from '../../../components/Checker/components';
 
 	onMount(() => {
-		const currentGpx = get(gpx);
-		if (currentGpx.features.length === 0) {
-			goto(`/${$page.params.round}/`);
-			return;
-		}
+		// const currentGpx = get(gpx);
+		// if (currentGpx.features.length === 0) {
+		// 	goto(`/${$page.params.round}/`);
+		// 	return;
+		// }
 		isOpen.set(true);
 	});
 </script>
