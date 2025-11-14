e443d9362cb4e664172c00fab98116952de68d53
Dan Nisbet <dan.nisbet@immense.ai>
2025-11-14 16:49:48 +0000

    fix(ratification): redirect to round page if no GPX features are available

--- src/routes/[round=name]/ratification/+page.svelte
+++ src/routes/[round=name]/ratification/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
+	import { get } from 'svelte/store';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import { isOpen } from '$lib/stores/checker.store';
 	import { gpx } from '$lib/stores/gpx.store';
 	import { Results } from '../../../components/Checker/components';
@@ -15,7 +18,14 @@
 	});
 
 	onMount(() => {
-		if (hasGpx) isOpen.set(true);
+		const currentGpx = get(gpx);
+		if (currentGpx.features.length === 0) {
+			goto(`/${$page.params.round}/`);
+			return;
+		}
+		if (hasGpx) {
+			isOpen.set(true);
+		}
 	});
 </script>
 
