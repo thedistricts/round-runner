45236aaebddfbfd86f21ef6f6dfdaa77af2724db
Dan Nisbet <dan.nisbet@immense.ai>
2025-11-14 11:21:52 +0000

    chore: update dependencies and configuration for improved compatibility and performance

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,23 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		target: 'esnext'
+	},
+	optimizeDeps: {
+		include: ['comlink']
+	},
+	worker: {
+		format: 'es'
+	},
 	test: {
 		include: ['src/**/*.{test,spec,svelte.test}.{js,ts}'],
 		setupFiles: ['./setupTests.ts'],
 		environment: 'jsdom', 
-	}
+	},
+	resolve: process.env.VITEST
+		? {
+				conditions: ['browser']
+			}
+		: undefined
 });
